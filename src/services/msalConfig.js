49f8aa9c42c46d3a01c5fcbf343f0d4e1c9be5a7
deepak31 <deepak.it22@bitsathy.ac.in>
2026-06-15 16:45:09 +0530

    Securely move MSAL IDs to env vars and configure GitHub Actions build

--- src/services/msalConfig.js
+++ src/services/msalConfig.js
@@ -2,8 +2,8 @@ import { PublicClientApplication } from "@azure/msal-browser";
 
 const msalConfig = {
     auth: {
-        clientId: "12dccfb2-944f-4d6d-85d2-4d1acc041b23",
-        authority: "https://login.microsoftonline.com/1e499b00-b34d-4461-b219-a45d602ca97c",
+        clientId: import.meta.env.VITE_MSAL_CLIENT_ID,
+        authority: `https://login.microsoftonline.com/${import.meta.env.VITE_MSAL_TENANT_ID}`,
         redirectUri: window.location.origin, // Automatically uses localhost or the Azure URL
     },
     cache: {
